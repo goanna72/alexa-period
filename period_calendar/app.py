3604b4c849943ef9bf6a2a1257e6e2f42b7c92d5
Anna-Lissa Sayers <annalissa@gmail.com>
2021-10-28 18:48:46 +1100

    Fixed indentation, removed check_delete

--- period_calendar/app.py
+++ period_calendar/app.py
@@ -8,7 +8,7 @@ from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from datetime import datetime, date
-
+import json
     
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
@@ -74,39 +74,36 @@ class DeletePeriodIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         
         try:            
-            check_delete = handler_input.request_envelope.request.intent.slots['delete'].value
             confirm_status = handler_input.request_envelope.request.intent.confirmation_status.value
-
-            if check_delete == 'YES' and confirm_status == 'CONFIRMED':
+            speech_text = "No deletion of data actioned "
+            if confirm_status == 'CONFIRMED':
                 import ask_sdk_core
                 user_id = ask_sdk_core.utils.request_util.get_user_id(handler_input)    
                 dyndb = boto3.resource('dynamodb', region_name='ap-southeast-2')
                 table = dyndb.Table('Menstruation')  
 
                 data = table.query(
-                KeyConditions={
-                'UserID': {
-                'AttributeValueList': [user_id],
-                'ComparisonOperator': 'EQ'
-                },
-            })
+                    KeyConditions={
+                    'UserID': {
+                    'AttributeValueList': [user_id],
+                    'ComparisonOperator': 'EQ'
+                    },
+                })
 
-            if data['Count'] == 0:
-                speech_text = "There is no data to delete."
-            else :
-                records = data["Items"]
+                if data['Count'] == 0:
+                    speech_text = "There is no data to delete."
+                else :
+                    records = data["Items"]
 
-                for r in range(len(records)):
-                    session_id = records[r]['SessionID']
-                    table.delete_item(
-                    Key={
-                    'UserID': user_id,
-                    'SessionID': session_id
-                    }
-                )
-                speech_text = "Your data has been deleted"
-            if check_delete == 'NO':
-                speech_text = "No deletion of data actioned "
+                    for r in range(len(records)):
+                        session_id = records[r]['SessionID']
+                        table.delete_item(
+                        Key={
+                        'UserID': user_id,
+                        'SessionID': session_id
+                        }
+                    )
+                    speech_text = "Your data has been deleted"
                             
                   
         except BaseException as e:
@@ -158,110 +155,110 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
 
-    fake_event = {
-        "version": "1.0",
-        "session": {
-            "new": False,
-            "sessionId": "amzn1.echo-api.session.efbf53be-7355-4c40-ac9e-ee450bf6adbe",
-            "application": {
-                "applicationId": "amzn1.ask.skill.358e56e6-230e-417e-bec1-0f88525e8bae"
-            },
-            "attributes": {},
-            "user": {
-                "userId": "amzn1.ask.account.AH5RVETXN5X23GWXRZZN6RFKRERM2XZPVFKTYQFSOJETMTI5LI3FGFUKDCFDYIMZIXZYBZRDK456SKM2WQBE7KGODUGIKYVIFOVJAYIKLKOSMS3VSEQHSZGVMEQBWXBKCUXUMLP3ANNQ2QLYHLJQKX7BDYTZC5CRCWLYHK3NE2BYXY33RZPJULVY2CDFFOK5NIWP3MKZ7YB5HBI"
-            }
-        },
-        "context": {
-            "Viewports": [
-                {
-                    "type": "APL",
-                    "id": "main",
-                    "shape": "RECTANGLE",
-                    "dpi": 213,
-                    "presentationType": "STANDARD",
-                    "canRotate": False,
-                    "configuration": {
-                        "current": {
-                            "mode": "HUB",
-                            "video": {
-                                "codecs": [
-                                    "H_264_42",
-                                    "H_264_41"
-                                ]
-                            },
-                            "size": {
-                                "type": "DISCRETE",
-                                "pixelWidth": 1280,
-                                "pixelHeight": 800
-                            }
-                        }
-                    }
-                }
-            ],
-            "Viewport": {
-                "experiences": [
-                    {
-                        "arcMinuteWidth": 346,
-                        "arcMinuteHeight": 216,
-                        "canRotate": False,
-                        "canResize": False
-                    }
-                ],
-                "mode": "HUB",
-                "shape": "RECTANGLE",
-                "pixelWidth": 1280,
-                "pixelHeight": 800,
-                "dpi": 213,
-                "currentPixelWidth": 1280,
-                "currentPixelHeight": 800,
-                "touch": [
-                    "SINGLE"
-                ],
-                "video": {
-                    "codecs": [
-                        "H_264_42",
-                        "H_264_41"
-                    ]
-                }
-            },
-            "Extensions": {
-                "available": {
-                    "aplext:backstack:10": {}
-                }
-            },
-            "System": {
-                "application": {
-                    "applicationId": "amzn1.ask.skill.358e56e6-230e-417e-bec1-0f88525e8bae"
-                },
-                "user": {
-                    "userId": "amzn1.ask.account.AH5RVETXN5X23GWXRZZN6RFKRERM2XZPVFKTYQFSOJETMTI5LI3FGFUKDCFDYIMZIXZYBZRDK456SKM2WQBE7KGODUGIKYVIFOVJAYIKLKOSMS3VSEQHSZGVMEQBWXBKCUXUMLP3ANNQ2QLYHLJQKX7BDYTZC5CRCWLYHK3NE2BYXY33RZPJULVY2CDFFOK5NIWP3MKZ7YB5HBI"
-                },
-                "device": {
-                    "deviceId": "amzn1.ask.device.AHZN3BI2ZS5OZCS4VWFUKTUVPUJNFRGVOQ5I5S7HEHCV6BB3U2CHD6U6CVZGNP4GNELFNXXCUTND52QTW2VODBHP67NVBXKCTM4DRPJZBZYMRSVCFATWTIWZQ2Y6267LR4HFB3DQ3EFTTIW4MULOXYFIRE63W6Q2BS367EHM6S5NRZSCDYU76",
-                    "supportedInterfaces": {}
-                },
-                "apiEndpoint": "https://api.fe.amazonalexa.com",
-                "apiAccessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5mZS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLjM1OGU1NmU2LTIzMGUtNDE3ZS1iZWMxLTBmODg1MjVlOGJhZSIsImV4cCI6MTYzNTIxNTkzMCwiaWF0IjoxNjM1MjE1NjMwLCJuYmYiOjE2MzUyMTU2MzAsInByaXZhdGVDbGFpbXMiOnsiY29udGV4dCI6IkFBQUFBQUFBQXdBTGxiMVBWSTE0WkZxMzBKdzZ3Yk1XS2dFQUFBQUFBQURuQkZxZlNRSEMzT0k0eVJ3K01Ibi9RZW9VU0VkbUNIcis2dTJ0bngxUWxQWSttdldpSnZrelc3OHcxTWMwUHVPY1psNlA1NjFUSUxOZVAxY3JTQVEvM2QyRlBua0F6V3FsTkRQZW44ZEJCSHRycVN3b1hldml6SEp3cEs2QVdIZFk5SVNqTEUxYkZhbHBpcjJGMTdMN2tTL2kvK0F3WC9jWUlSeFR6R3ZBNU9KbnlnNE9OVEl1TW8yUnNTZGY1WllKTlN5WUptQlUvczRIYXU4T0E2b2xsM0NGYVB2WkdOcENVdFBlaW9nMmZmcFIvNHhjNloxeEEzbWpjWXYyYUphMTNGWmtGN01CaHgvdXNwcFhuL1JqQkdZRWJ0cVJFS1h3ZDMrcU5LTmFiYXJ4bFJkdXFGQ2FHaWFveDg4aGkwN0JiZTc0cWJyZHdoSWo0RFVNeUNjVHFaMkl2c0RNTkhXdGM0d2Z5NCtVSWlaN0oreWtaSWRiS0gyN2Y5NE45SkRTMDNCQmJEZ054b25XIiwiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUhaTjNCSTJaUzVPWkNTNFZXRlVLVFVWUFVKTkZSR1ZPUTVJNVM3SEVIQ1Y2QkIzVTJDSEQ2VTZDVlpHTlA0R05FTEZOWFhDVVRORDUyUVRXMlZPREJIUDY3TlZCWEtDVE00RFJQSlpCWllNUlNWQ0ZBVFdUSVdaUTJZNjI2N0xSNEhGQjNEUTNFRlRUSVc0TVVMT1hZRklSRTYzVzZRMkJTMzY3RUhNNlM1TlJaU0NEWVU3NiIsInVzZXJJZCI6ImFtem4xLmFzay5hY2NvdW50LkFINVJWRVRYTjVYMjNHV1hSWlpONlJGS1JFUk0yWFpQVkZLVFlRRlNPSkVUTVRJNUxJM0ZHRlVLRENGRFlJTVpJWFpZQlpSREs0NTZTS00yV1FCRTdLR09EVUdJS1lWSUZPVkpBWUlLTEtPU01TM1ZTRVFIU1pHVk1FUUJXWEJLQ1VYVU1MUDNBTk5RMlFMWUhMSlFLWDdCRFlUWkM1Q1JDV0xZSEszTkUyQllYWTMzUlpQSlVMVlkyQ0RGRk9LNU5JV1AzTUtaN1lCNUhCSSJ9fQ.yBc6qpt4uNnhrr-pur9QC6pbsa4xQCjiGCxlKmOlf254yUmg1mzPvCWa5luGsw7sCOSKDaEJXSxtt_mB-bWree4OhhOo49JwwKFoWfGDnorH0WS_VlBU_vQqKjfamnBJTk1UxzV_xOIFCnYo7iksyHINA0JC1WfdUX3_P6y5oFFn_CCrcBs2OupklUDBSSr2J9o3aczTtrFNygAKyt9y5KV2QPFazb4A-snjwvvdTOJ8Aso0VVfGuh6kuTXNRmOYc5F_mwtAe_ceSxIJKkXo-uUBkEKs97bejBiCxoxi8OsmhXnvMbjf0Cyu_upGHQgT1wA3AGoAbZH932f_swnprg"
-            }
-        },
-        "request": {
-            "type": "IntentRequest",
-            "requestId": "amzn1.echo-api.request.8aa0e913-b8b1-452b-b58e-573e2a504fc6",
-            "locale": "en-US",
-            "timestamp": "2021-10-26T02:33:50Z",
-            "intent": {
-                "name": "DeletePeriod",
-                "confirmationStatus": "CONFIRMED",
-                "slots": {
-                    "delete": {
-                        "name": "delete",
-                        "value": "YES",
-                        "confirmationStatus": "NONE",
-                        "source": "USER"
-                    }
-                }
-            }
-        }
-    }
+    fake_event = """{
+	"version": "1.0",
+	"session": {
+		"new": false,
+		"sessionId": "amzn1.echo-api.session.b87ac02f-cf3b-4266-97ab-b5df93bac683",
+		"application": {
+			"applicationId": "amzn1.ask.skill.358e56e6-230e-417e-bec1-0f88525e8bae"
+		},
+		"attributes": {},
+		"user": {
+			"userId": "amzn1.ask.account.AH5RVETXN5X23GWXRZZN6RFKRERM2XZPVFKTYQFSOJETMTI5LI3FGFUKDCFDYIMZIXZYBZRDK456SKM2WQBE7KGODUGIKYVIFOVJAYIKLKOSMS3VSEQHSZGVMEQBWXBKCUXUMLP3ANNQ2QLYHLJQKX7BDYTZC5CRCWLYHK3NE2BYXY33RZPJULVY2CDFFOK5NIWP3MKZ7YB5HBI"
+		}
+	},
+	"context": {
+		"Viewports": [
+			{
+				"type": "APL",
+				"id": "main",
+				"shape": "RECTANGLE",
+				"dpi": 213,
+				"presentationType": "STANDARD",
+				"canRotate": false,
+				"configuration": {
+					"current": {
+						"mode": "HUB",
+						"video": {
+							"codecs": [
+								"H_264_42",
+								"H_264_41"
+							]
+						},
+						"size": {
+							"type": "DISCRETE",
+							"pixelWidth": 1280,
+							"pixelHeight": 800
+						}
+					}
+				}
+			}
+		],
+		"Viewport": {
+			"experiences": [
+				{
+					"arcMinuteWidth": 346,
+					"arcMinuteHeight": 216,
+					"canRotate": false,
+					"canResize": false
+				}
+			],
+			"mode": "HUB",
+			"shape": "RECTANGLE",
+			"pixelWidth": 1280,
+			"pixelHeight": 800,
+			"dpi": 213,
+			"currentPixelWidth": 1280,
+			"currentPixelHeight": 800,
+			"touch": [
+				"SINGLE"
+			],
+			"video": {
+				"codecs": [
+					"H_264_42",
+					"H_264_41"
+				]
+			}
+		},
+		"Extensions": {
+			"available": {
+				"aplext:backstack:10": {}
+			}
+		},
+		"System": {
+			"application": {
+				"applicationId": "amzn1.ask.skill.358e56e6-230e-417e-bec1-0f88525e8bae"
+			},
+			"user": {
+				"userId": "amzn1.ask.account.AH5RVETXN5X23GWXRZZN6RFKRERM2XZPVFKTYQFSOJETMTI5LI3FGFUKDCFDYIMZIXZYBZRDK456SKM2WQBE7KGODUGIKYVIFOVJAYIKLKOSMS3VSEQHSZGVMEQBWXBKCUXUMLP3ANNQ2QLYHLJQKX7BDYTZC5CRCWLYHK3NE2BYXY33RZPJULVY2CDFFOK5NIWP3MKZ7YB5HBI"
+			},
+			"device": {
+				"deviceId": "amzn1.ask.device.AHZN3BI2ZS5OZCS4VWFUKTUVPUJNFRGVOQ5I5S7HEHCV6BB3U2CHD6U6CVZGNP4GNELFNXXCUTND52QTW2VODBHP67NVBXKCTM4DRPJZBZYMRSVCFATWTIWZQ2Y6267LR4HFB3DQ3EFTTIW4MULOXYFIRE63W6Q2BS367EHM6S5NRZSCDYU76",
+				"supportedInterfaces": {}
+			},
+			"apiEndpoint": "https://api.fe.amazonalexa.com",
+			"apiAccessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5mZS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLjM1OGU1NmU2LTIzMGUtNDE3ZS1iZWMxLTBmODg1MjVlOGJhZSIsImV4cCI6MTYzNTQwNjUyOCwiaWF0IjoxNjM1NDA2MjI4LCJuYmYiOjE2MzU0MDYyMjgsInByaXZhdGVDbGFpbXMiOnsiY29udGV4dCI6IkFBQUFBQUFBQXdBMEl5YTFidTVhNlY2M3hTV2JEK3N6S2dFQUFBQUFBQUNtTHZ2VlZHb0RVSWlreXR2WlFpc0lWS3U4RDJWTzlRaktQNnBCQ21WTUdhT08vbkZXWkYweGE1TWNOa3B0dnJJVWhFMXV3bkF2anc5Yi85UUtnb3NkRWlWemJ0am9WaE5ZaHl5ZW5MYXJYWGpxaW8vTmhpamhsSnE3NFgvU0tNaVRnWEtOOWJNVHN5SCtiM3hzaE9lNjRwNXFmakxJV1l0WUtPOW9UNkozZ2drQ0Q5YkVnV2swZUxUaWVMOXFYVzE1TllwSFRqa0lrYkYxdDQ5eGYyMk5iRWZ4T1BUYUZiMUpoT2wwakY5d0M5cFUrWUFXV0dQMUZ3YmwzZ3Voakt1Nnp0ZGdISXRwWHkwVVVsU0tWaWVsRUxOdG9zZGtPZVgwNW5raTN1WEJEcEZ5UnEwVGVSZGJHVllpOVU3eU1xQUJVNTRVWWNKMnFOL2djeHZQeGQ3djVqY09sYTdYSHhLM1hMU2VzQjhGZkNITU9FV1JadzdPM1NXMjFDZWxuQ1U2dnUwc1duak1pekJEIiwiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUhaTjNCSTJaUzVPWkNTNFZXRlVLVFVWUFVKTkZSR1ZPUTVJNVM3SEVIQ1Y2QkIzVTJDSEQ2VTZDVlpHTlA0R05FTEZOWFhDVVRORDUyUVRXMlZPREJIUDY3TlZCWEtDVE00RFJQSlpCWllNUlNWQ0ZBVFdUSVdaUTJZNjI2N0xSNEhGQjNEUTNFRlRUSVc0TVVMT1hZRklSRTYzVzZRMkJTMzY3RUhNNlM1TlJaU0NEWVU3NiIsInVzZXJJZCI6ImFtem4xLmFzay5hY2NvdW50LkFINVJWRVRYTjVYMjNHV1hSWlpONlJGS1JFUk0yWFpQVkZLVFlRRlNPSkVUTVRJNUxJM0ZHRlVLRENGRFlJTVpJWFpZQlpSREs0NTZTS00yV1FCRTdLR09EVUdJS1lWSUZPVkpBWUlLTEtPU01TM1ZTRVFIU1pHVk1FUUJXWEJLQ1VYVU1MUDNBTk5RMlFMWUhMSlFLWDdCRFlUWkM1Q1JDV0xZSEszTkUyQllYWTMzUlpQSlVMVlkyQ0RGRk9LNU5JV1AzTUtaN1lCNUhCSSJ9fQ.Z1wUXS2od3PvZVkA7gW34RsZEI1lINQiyuRiZaI_xVtLxlE4zdn7-agIwOCy731mVXfk5lPN1ry5yKGgYuNZLDlxACNXpLDBAMf7LdA4uOXhKkUz3cQ9SBEDThOqfxuQB5LxQNajzP2Yor9gUD4cVsGOLde9x8FxlcHtADuyU90gYYj5Rb2FGjkJUktqrnDRMprGUjBWkE4jY7cEbfFuDH7r6qCryKAZ8O699LerdONFI7fFCfhn4r-DMjaPeksdFg_XZm08LHtU0BnNlaEONj8XwWCCEJiZIIUp5ze8nfXpcc4KTZzTMwRB5fUusMcAQ-elZJuWctaGeaPifY035A"
+		}
+	},
+	"request": {
+		"type": "IntentRequest",
+		"requestId": "amzn1.echo-api.request.d130214b-f58d-4e54-83d6-54c453b92da0",
+		"locale": "en-US",
+		"timestamp": "2021-10-28T07:30:28Z",
+		"intent": {
+			"name": "DeletePeriod",
+			"confirmationStatus": "CONFIRMED",
+			"slots": {
+				"delete": {
+					"name": "delete",
+					"confirmationStatus": "NONE",
+					"source": "USER"
+				}
+			}
+		},
+		"dialogState": "COMPLETED"
+	}
+}"""
 
-    sb.lambda_handler()(fake_event, None)
+    sb.lambda_handler()(json.loads(fake_event), None)
